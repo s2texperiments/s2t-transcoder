d7fd7e0c9674e4787e34c686efdf6dd3d233075f
Stefan Breitenstein <stefan.breitenstein@gmail.com>
2018-05-17 00:28:44 +0200

    init

---
+++ index.js
@@ -0,0 +1,54 @@
+process.env.PATH = process.env.PATH + ':/tmp/';
+process.env['FFMPEG_PATH'] = '/tmp/ffmpeg';
+const BIN_PATH = process.env['LAMBDA_TASK_ROOT'];
+process.env['PATH'] = process.env['PATH'] + ':' + BIN_PATH;
+
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const writeFile = util.promisify(require('fs').writeFile);
+const readFile = util.promisify(require('fs').readFile);
+
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+const readS3 = async (params) =>
+    new Promise((resolve, rejected) =>
+        s3.getObject(params, (err, data) =>
+            err ? rejected(err) : resolve(data)));
+
+const putS3 = async (params) =>
+    new Promise((resolve, rejected) =>
+        s3.putObject(params, (err, data) =>
+            err ? rejected(err) : resolve(data)));
+
+
+const collectFFMpegCodec = (codec) => {
+    switch (codec) {
+        case "opus":
+            return "libopus"
+        default:
+            throw "unknown codec"
+    }
+}
+
+const dataPath = "/tmp/data";
+const storeInPath = `${dataPath}/in`;
+const storeOutPath = `${dataPath}/out`;
+
+exports.handler = async (event, context, callback) => {
+
+    await exec(`mkdir -p ${dataPath}`);
+
+    //    let result = await readS3({ Bucket: event.in.bucket, Key: event.in.key });
+    let s3In = await readS3({ Bucket: event.in.bucket, Key: event.in.key });
+    await writeFile(storeInPath, s3In.Body);
+
+    //ffmpeg mock
+    await exec(`cp /var/task/ffmpeg/ffmpeg /tmp/.; chmod 755 /tmp/ffmpeg;`);
+    await exec(`/tmp/ffmpeg -y -i ${storeInPath} -acodec ${collectFFMpegCodec(event.codecOut)} -mode mono -ac 1 ${storeOutPath}`);
+
+    let s3Out = await readFile(storeOutPath);
+    await putS3({ Bucket: event.out.bucket, Key: event.out.key, Body: s3Out });
+
+
+    callback(null, null);
+};
